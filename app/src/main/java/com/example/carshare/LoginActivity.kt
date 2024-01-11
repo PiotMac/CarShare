3bcd20b60581f4c9640b36840931b436ebfc9783
Szymon Pestrowicz <pestrowicz.szymon@gmail.com>
2024-01-11 10:48:22 +0100

    poprawki logowania, zostalo jeszcze dodanie informacji ktora bedzie przesylana do mainactivity z informacja ktory uzytkownik sie zalogowal

--- app/src/main/java/com/example/carshare/LoginActivity.kt
+++ app/src/main/java/com/example/carshare/LoginActivity.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.example.carshare.MainActivity
 import com.example.carshare.R
 import com.example.carshare.database.SQLiteHelper
@@ -28,24 +29,30 @@ class LoginActivity : AppCompatActivity() {
     'result':
         'LOGGED_IN' - everything worked fine, second value is 'name'
         'WRONG_PASSWORD'
-        'WRONG_EMAIL' - email not in database, second value is 'email'
+        'WRONG_PHONE' - phone not in database, second value is 'email'
      */
     fun onLoginClick(view: View) {
         val intent = Intent(this, MainActivity::class.java)
         if(sqliteHelper.isPhoneAlreadyUsed(editPhone.text.toString())) {
             if(sqliteHelper.checkIfUserExists(editPassword.text.toString(), editPhone.text.toString())) {
+                val name = sqliteHelper.getUserFirstnameFromPhone(editPhone.text.toString())
                 intent.putExtra("result", "LOGGED_IN")
                 intent.putExtra("name", sqliteHelper.getUserFirstnameFromPhone(editPhone.text.toString()))
+                Toast.makeText(this, "Welcome, $name", Toast.LENGTH_SHORT).show()
+                startActivity(intent)
             } else {
                 intent.putExtra("result", "WRONG_PASSWORD")
+                Toast.makeText(this, "Wrong password", Toast.LENGTH_SHORT).show()
             }
         } else {
-            intent.putExtra("result", "WRONG_EMAIL")
+            val phone = editPhone.text.toString()
+            intent.putExtra("result", "WRONG_PHONE")
             intent.putExtra("email", editPhone.text.toString())
+            Toast.makeText(this, "User $phone doesn't exist", Toast.LENGTH_SHORT).show()
         }
-        setResult(RESULT_OK, intent)
+        //setResult(RESULT_OK, intent)
         //finish()
-        startActivity(intent)
+        //startActivity(intent)
     }
 
     fun onRegClick(view: View) {
